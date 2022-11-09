e2fba59cf36cdb6aa38640bf3e1571b63c5140e8
filip.zwolinski <filip.zwolinski@riskmethods.net>
2022-11-09 14:33:00 +0100

    WIP

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>NEW TEST TEST</p>
+        <p>NEW TEST TEST TEST</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
